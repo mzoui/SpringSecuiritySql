3f0e4ac78ea3e8d730967593da05e3cf620f96bd
mzoui <mah_zouihed@yahoo.fr>
2019-07-07 12:49:36 -0400

    add test

--- springAuthenticationExemple/src/test/java/mah/auth/ex/SpringAuthenticationExempleApplicationTests.java
+++ springAuthenticationExemple/src/test/java/mah/auth/ex/SpringAuthenticationExempleApplicationTests.java
@@ -1,5 +1,7 @@
 package mah.auth.ex;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ public class SpringAuthenticationExempleApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertEquals("", "");
 	}
 
 }
